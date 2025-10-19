f08734a0cee0e7ee41c92a8cfb10e1b68750ca63
Mykhailo Bodnar <bodnar.mikhail@gmail.com>
2025-10-19 11:20:27 +0200

    added faq-section

--- scripts/main.js
+++ scripts/main.js
@@ -485,4 +485,26 @@ mobileMenuLinks.forEach(function (link) {
     updateTestimonials();
     startAutoPlay();
 
+    const faqItems = document.querySelectorAll('.faq-item');
+    
+    faqItems.forEach(item => {
+        const question = item.querySelector('.faq-question');
+        
+        question.addEventListener('click', () => {
+            const isActive = item.classList.contains('active');
+            
+            faqItems.forEach(otherItem => {
+                if (otherItem !== item) {
+                    otherItem.classList.remove('active');
+                }
+            });
+            
+            if (!isActive) {
+                item.classList.add('active');
+            } else {
+                item.classList.remove('active');
+            }
+        });
+    });
+
 });
